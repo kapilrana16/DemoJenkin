80f5e22fe7f0a228c47f5e751324a5ed4c9e48c7
kapil.rana <kapil@os-kapil.icpl.local>
2017-02-15 19:22:11 +0530

    first commitcommiting test project

---
+++ src/com/qa/example/testpkg1/Login.java
@@ -0,0 +1,13 @@
+package com.qa.example.testpkg1;
+
+
+public class Login {
+	
+	
+	public void display()
+	{
+		System.out.println("This is test project");
+	}
+
+}
+
---
+++ src/com/qa/example/testpkg2/Base.java
@@ -0,0 +1,23 @@
+package com.qa.example.testpkg2;
+
+
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.qa.example.testpkg1.Login;
+
+
+
+public class Base {
+
+
+	
+	@Test
+	public void m1()
+	{
+		Login objLogin = new Login();
+		objLogin.display();
+	}
+
+}
